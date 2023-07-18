ee14a68ee570a0e54c83d073eea0d1561a5aa006
Inalia Ashanti <inaliaashanti@Inalias-MacBook-Air.local>
2023-07-17 20:37:50 -0700

    worked with TA first server side

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+// import SignUp from "./pages/SignUp";
+import Home from "./pages/Home.js";
+import { Divider } from '@material-ui/core';
+
+function App() {
+  const httpLink = createHttpLink({
+    uri: '/graphql',
+  });
+  
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('id_token');
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+  
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+  return 
+(
+  <ApolloProvider client={client}>
+
+  {/* // <Router>
+  //       <Routes>
+  //         <Route path="/" element={<Home />} />
+  //       </Routes>
+  // </Router> */}
+  
+<div>
+  <h1>Home</h1>
+</div>
+   </ApolloProvider>
+ )
+}
+export default App;
