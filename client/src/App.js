f645c3673171d4e5c10ae89a04f2c46d511341da
Radhika Bandi <radhika.shetti@gendigital.com>
2023-07-18 20:00:04 -0700

    update login page with material UI

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SignUp from "./pages/SignUp.js";
 import Home from "./pages/Home.js";
+import Login from "./pages/Login.js";
 import { Divider } from '@material-ui/core';
 
 function App() {
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link, TextField, Button, Typography, Box } from '@mui/material';
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -31,43 +33,56 @@ function Login(props) {
     });
   };
 
-  return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+  
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Link to="/signup" variant="body2">← Go to Signup</Link>
+  
+      <Typography variant="h2" component="h2" sx={{ my: 2 }}>
+        ShopCyber Login
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleFormSubmit}
+        sx={{ width: '100%', maxWidth: 360 }}
+      >
+        <Box sx={{ my: 2 }}>
+          <TextField
+            fullWidth
             placeholder="youremail@test.com"
+            label="Email address"
             name="email"
             type="email"
             id="email"
             onChange={handleChange}
           />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
+        </Box>
+        <Box sx={{ my: 2 }}>
+          <TextField
+            fullWidth
             placeholder="******"
+            label="Password"
             name="password"
             type="password"
             id="pwd"
             onChange={handleChange}
           />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+        </Box>
+        {error && (
+          <Box sx={{ my: 2 }}>
+            <Typography variant="body1" color="error">
+              The provided credentials are incorrect
+            </Typography>
+          </Box>
+        )}
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
-}
-
+        }
 export default Login;
