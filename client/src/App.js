48268ebea6f88e7b9126b1201f4032d4a95ed0d5
Inalia Ashanti <inaliaashanti@Inalias-MacBook-Air.local>
2023-07-18 19:29:52 -0700

    changed ts to js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import SignUp from "./pages/SignUp.tsx";
+import SignUp from "./pages/SignUp.js";
 import Home from "./pages/Home.js";
 import { Divider } from '@material-ui/core';
 
